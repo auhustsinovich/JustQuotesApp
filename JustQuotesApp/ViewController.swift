0778939abc74b39b78dbeb9ef3689786dc3b9eae
auhustsinovich <avgustinovich4@gmail.com>
2022-09-18 17:16:35 +0200

    Initial Commit

---
+++ JustQuotesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JustQuotesApp
+//
+//  Created by Daniil Auhustsinovich on 18.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
